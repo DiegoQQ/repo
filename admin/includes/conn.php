f59e6b990252185943c510d8173771f2c0f8cf42
Diego Quintero <quicenoquinterodiego@gmail.com>
2023-05-22 21:01:46 -0500

    cambios

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost", "root", "", 'apsystem');
+	$conn = new mysqli("localhost", "admin", "1803", 'apsystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
